88f589601d0a4e5331919effe2abf2e7bee1fd77
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-07-27 12:30:45 +0530

    Update and rename Leap.java to Hello.java

--- Hello.java
+++ Hello.java
@@ -5,10 +5,9 @@ class Greater
 	public static void main (String[] args) throws java.lang.Exception
 	{
 			Scanner sc=new Scanner(System.in);
-		long year=sc.nextLong();
-		if(year%4==0)
-			System.out.println("yes");
-			else
-			System.out.println("no");	
+		long N=sc.nextLong();
+		for(int i=0;i<N;i++)
+			System.out.println("Hello");
+			
 	}
 }
