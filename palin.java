4e00ccfa06c1e54acd7f96a7370fe7a35c682544
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-07-27 16:16:10 +0530

    Update and rename power.java to palin.java

---
+++ palin.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Value
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+				Scanner sc=new Scanner(System.in);
+	                       	                        int dup1=num,dup=0,rem=0;
+	                        while(num>0)
+	                        {
+	                        		rem=num%10;
+	                        		dup=dup*10+rem;
+	                        		num=num/10;
+	                       }
+	                       if(dup1==dup)
+	                        System.out.println("yes");
+	                        else
+	                     System.out.println("no");
+	}
+}
