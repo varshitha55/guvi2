6c185cdb015b19c54353f366bd5a69fdb12f42a6
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-10-02 15:15:06 +0530

    Update and rename small21.java to ksmalln1.java

--- ksmalln1.java
+++ ksmalln1.java
@@ -5,12 +5,13 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class small21
+class ksmalln1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
+		int k=sc.nextInt();
 		int[] a=new int[n];
 		for(int i=0;i<n;i++)
 			a[i]=sc.nextInt();
@@ -26,6 +27,7 @@ class small21
 				}
 			}
 		}
-		System.out.print(a[1]);
+		System.out.print(a[--k]);
+		
 	}
 }
