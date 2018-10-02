8014dfa784466868258dc384f1aa59e43d93308c
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-10-02 15:22:15 +0530

    Update and rename strf1.java to stroccn1.java

--- stroccn1.java
+++ stroccn1.java
@@ -5,20 +5,21 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class strf1
+class stroccn1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		String str=sc.next();
 		char c=sc.next().charAt(0);
+		int count=0;
 		for(int i=0;i<str.length();i++)
 		{
 			if(str.charAt(i)==c)
 			{
-			System.out.println(++i);
-			break;
+			count++;
 			}
 		}
+		System.out.println(count);
 	}
 }
