fa6222d173bad7bbd78a060506770cc1767e573d
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-10-02 15:16:20 +0530

    Update and rename ksmalln1.java to strlen1.java

--- strlen1.java
+++ strlen1.java
@@ -5,29 +5,18 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class ksmalln1
+class strlen1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int k=sc.nextInt();
-		int[] a=new int[n];
-		for(int i=0;i<n;i++)
-			a[i]=sc.nextInt();
-		for(int i=0;i<n;i++)
+		String str=sc.nextLine();
+		str += '\0';
+		int count=0;
+		for(int i=0;str.charAt(i)!='\0';i++)
 		{
-			for(int j=i+1;j<n-i-1;j++)
-			{
-				if(a[i]>a[j])
-				{
-					int tmp=a[i];
-					a[i]=a[j];
-					a[j]=tmp;
-				}
-			}
+			count++;
 		}
-		System.out.print(a[--k]);
-		
+		System.out.println(count);
 	}
 }
