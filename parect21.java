129854e130e6c3fc42aa7719af2e569988df0a72
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-10-02 15:10:11 +0530

    Update and rename stringcont1.java to parect21.java

--- parect21.java
+++ parect21.java
@@ -5,24 +5,29 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class stringcont1
+class parect21
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		String str1=sc.next();
-		String str2=sc.next();
+		int p=sc.nextInt();
+		int a=sc.nextInt();
 		int count=0;
-		if(str2.length()>str1.length())
+		p /= 2;
+		for(int i=1;i<=a;i++)
 		{
-			System.out.print("no");
+			for(int j=1;j<=p;j++)
+			{
+				if(i+j==p && i*j==a)
+				{
+					count=1;
+					break;
+				}
+			}
 		}
+		if(count==1)
+		System.out.println("yes");
 		else
-		{
-				if(str1.contains(str2))
-					System.out.print("yes");
-				else
-					System.out.print("no");	
-		}
+		System.out.println("no");
 	}
 }
