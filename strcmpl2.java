161faf4104261674a24d913a3079929db35d5b7f
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-10-02 15:19:42 +0530

    Update and rename str121.java to strcmpl2.java

--- strcmpl2.java
+++ strcmpl2.java
@@ -5,32 +5,23 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class str121
+class strcmpl2
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		String str1=sc.next();
 		String str2=sc.next();
-		int count=0;
-		if(str1.length()!=str2.length())
+		String str11=str1.toLowerCase();
+		String str22=str2.toLowerCase();
+		if(str11.length()!=str22.length())
 			System.out.println("no");
-		else 
+		else
 		{
-			for(int i=0;i<str1.length();i++)
-			{
-				if(str1.charAt(i)==str2.charAt(i))
-				{
-					
-				}
-				else
-					count++;
-			}
-			if(count==0)
+			if(str11.equals(str22))
 			System.out.println("yes");
 			else
 			System.out.println("no");
 		}
-		
 	}
 }
