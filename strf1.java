a762a761894e29a9ecda93fad0f3918dc1306ac7
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-10-02 15:21:02 +0530

    Update and rename strcmpl2.java to strf1.java

--- strf1.java
+++ strf1.java
@@ -5,23 +5,20 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class strcmpl2
+class strf1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		String str1=sc.next();
-		String str2=sc.next();
-		String str11=str1.toLowerCase();
-		String str22=str2.toLowerCase();
-		if(str11.length()!=str22.length())
-			System.out.println("no");
-		else
+		String str=sc.next();
+		char c=sc.next().charAt(0);
+		for(int i=0;i<str.length();i++)
 		{
-			if(str11.equals(str22))
-			System.out.println("yes");
-			else
-			System.out.println("no");
+			if(str.charAt(i)==c)
+			{
+			System.out.println(++i);
+			break;
+			}
 		}
 	}
 }
