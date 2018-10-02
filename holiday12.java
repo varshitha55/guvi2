ea6a0c4fe0f472d7c0d9cb928c57d58b5a91a0fc
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-10-02 14:43:02 +0530

    Create holiday12.java

---
+++ holiday12.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class holiday12
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		switch(str)
+		{
+			case "Monday":System.out.println("no");
+				break;
+			case "Tuesday":System.out.println("no");
+				break;
+			case "Wednesday":System.out.println("no");
+				break;
+			case "Thursday":System.out.println("no");
+				break;
+			case "Friday":System.out.println("no");
+				break;
+			case "Saturday":System.out.println("yes");
+				break;
+			case "Sunday":System.out.println("yes");
+				break;
+		}
+	}
+}
